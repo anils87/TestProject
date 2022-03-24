056415c2002e0fa9d99cfc82b901ea02d9da6391
Anil Yadav <anils15787@gmail.com>
2022-03-24 11:10:27 +0530

    implemented xUnit test case example

--- SparkyXUnitTest/BankAccountXUnitTests.cs
+++ SparkyXUnitTest/BankAccountXUnitTests.cs
@@ -1,147 +1,144 @@
-﻿//using Moq;
-//using NUnit.Framework;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Moq;
 
-//namespace Sparky
-//{
-//    [TestFixture]
-//    public class BankAccountXUnitTests
-//    {
-//        // private BankAccount bankAccount;
-//        [SetUp]
-//        public void Setup()
-//        {
-//            //bankAccount = new BankAccount(new LogFakker());
-//        }
-//        //[Test]
-//        //public void BankDepositLogFakker_Add100_ReturnTrue()
-//        //{
-//        //    BankAccount bankAccount = new BankAccount(new LogFakker());
-//        //    var result = bankAccount.Deposit(100);
-//        //    Assert.IsTrue(result);
-//        //    Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
-//        //}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
-//        [Test]
-//        public void BankDeposit_Add100_ReturnTrue()
-//        {
-//            var logMock = new Mock<ILogBook>();
-//            logMock.Setup(x => x.Message(""));
-//            BankAccount bankAccount = new BankAccount(logMock.Object);
-//            var result = bankAccount.Deposit(100);
-//            Assert.IsTrue(result);
-//            Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
-//        }
-//        [Test]
-//        [TestCase(200, 100)]
-//        [TestCase(200, 150)]
-//        public void BankWithdraw_Withdraw100With200Balance_ReturnTrue(int balance, int withdraw)
-//        {
-//            var logMock = new Mock<ILogBook>();
-//            logMock.Setup(x => x.LogToDB(It.IsAny<string>())).Returns(true);
-//            logMock.Setup(x => x.LogBalanceAfterWithdrawal(It.Is<int>(x => x > 0))).Returns(true);
+namespace Sparky
+{
+    
+    public class BankAccountXUnitTests
+    {        
+        [Fact]
+        public void BankDepositLogFakker_Add100_ReturnTrue()
+        {
+            var logMock = new Mock<ILogBook>();
+            logMock.Setup(x => x.Message(""));
+            BankAccount bankAccount = new BankAccount(logMock.Object);
+            var result = bankAccount.Deposit(100);
+            Assert.True(result);
+            Assert.Equal(100,bankAccount.GetBalance());
+        }
 
-//            BankAccount bankAccount = new BankAccount(logMock.Object);
-//            bankAccount.Deposit(balance);
-//            var result = bankAccount.Withdraw(withdraw);
-//            Assert.IsTrue(result);
-//        }
+        [Fact]
+        public void BankDeposit_Add100_ReturnTrue()
+        {
+            var logMock = new Mock<ILogBook>();
+            logMock.Setup(x => x.Message(""));
+            BankAccount bankAccount = new BankAccount(logMock.Object);
+            var result = bankAccount.Deposit(100);
+            Assert.True(result);            
+            Assert.Equal<int>(100, bankAccount.GetBalance());
+        }
+        [Theory]
+        [InlineData(200, 100)]
+        [InlineData(200, 150)]
+        public void BankWithdraw_Withdraw100With200Balance_ReturnTrue(int balance, int withdraw)
+        {
+            var logMock = new Mock<ILogBook>();
+            logMock.Setup(x => x.LogToDB(It.IsAny<string>())).Returns(true);
+            logMock.Setup(x => x.LogBalanceAfterWithdrawal(It.Is<int>(x => x > 0))).Returns(true);
 
-//        [Test]
-//        [TestCase(200, 300)]
-//        public void BankWithdraw_Withdraw300With200Balance_ReturnFalse(int balance, int withdraw)
-//        {
-//            var logMock = new Mock<ILogBook>();
-//            logMock.Setup(x => x.LogBalanceAfterWithdrawal(It.Is<int>(x => x > 0))).Returns(true);
-//            //logMock.Setup(x => x.LogBalanceAfterWithdrawal(It.Is<int>(x => x <= 0))).Returns(false);
-//            logMock.Setup(x => x.LogBalanceAfterWithdrawal(It.IsInRange(int.MinValue, -1, Moq.Range.Inclusive))).Returns(false);
-//            BankAccount bankAccount = new BankAccount(logMock.Object);
-//            bankAccount.Deposit(balance);
-//            var result = bankAccount.Withdraw(withdraw);
-//            Assert.IsFalse(result);
-//        }
+            BankAccount bankAccount = new BankAccount(logMock.Object);
+            bankAccount.Deposit(balance);
+            var result = bankAccount.Withdraw(withdraw);
+            Assert.True(result);
+        }
 
-//        [Test]
+        [Theory]
+        [InlineData(200, 300)]
+        public void BankWithdraw_Withdraw300With200Balance_ReturnFalse(int balance, int withdraw)
+        {
+            var logMock = new Mock<ILogBook>();
+            logMock.Setup(x => x.LogBalanceAfterWithdrawal(It.Is<int>(x => x > 0))).Returns(true);
+            //logMock.Setup(x => x.LogBalanceAfterWithdrawal(It.Is<int>(x => x <= 0))).Returns(false);
+            logMock.Setup(x => x.LogBalanceAfterWithdrawal(It.IsInRange(int.MinValue, -1, Moq.Range.Inclusive))).Returns(false);
+            BankAccount bankAccount = new BankAccount(logMock.Object);
+            bankAccount.Deposit(balance);
+            var result = bankAccount.Withdraw(withdraw);
+            Assert.False(result);
+        }
 
-//        public void BankLogDummy_LogMockString_ReturnTrue()
-//        {
-//            var logMock = new Mock<ILogBook>();
-//            string desiredOutput = "hello";
-//            logMock.Setup(x => x.MessageWithReturnStr(It.IsAny<string>())).Returns((string str) => str.ToLower());
-//            Assert.That(logMock.Object.MessageWithReturnStr("HELLo"), Is.EqualTo(desiredOutput));
-//        }
+        [Fact]
 
-//        [Test]
+        public void BankLogDummy_LogMockString_ReturnTrue()
+        {
+            var logMock = new Mock<ILogBook>();
+            string desiredOutput = "hello";
+            logMock.Setup(x => x.MessageWithReturnStr(It.IsAny<string>())).Returns((string str) => str.ToLower());
+            Assert.Equal(desiredOutput,logMock.Object.MessageWithReturnStr("HELLo"));
+        }
 
-//        public void BankLogDummy_LogMockOutputString_ReturnTrue()
-//        {
-//            var logMock = new Mock<ILogBook>();
-//            string desiredOutput = "hello";
-//            logMock.Setup(x => x.LogWithOutputResult(It.IsAny<string>(), out desiredOutput)).Returns(true);
-//            string result = "";
-//            Assert.IsTrue(logMock.Object.LogWithOutputResult("Anil", out result));
-//            Assert.That(result, Is.EqualTo(desiredOutput));
-//        }
+        [Fact]
 
-//        [Test]
+        public void BankLogDummy_LogMockOutputString_ReturnTrue()
+        {
+            var logMock = new Mock<ILogBook>();
+            string desiredOutput = "hello";
+            logMock.Setup(x => x.LogWithOutputResult(It.IsAny<string>(), out desiredOutput)).Returns(true);
+            string result = "";
+            Assert.True(logMock.Object.LogWithOutputResult("Anil", out result));
+            Assert.Equal(desiredOutput,result);
+        }
 
-//        public void BankLogDummy_LogRefChecker_ReturnTrue()
-//        {
-//            var logMock = new Mock<ILogBook>();
-//            Customer customer = new();
-//            Customer customerNotUsed = new();
-//            logMock.Setup(x => x.LogWithRefObj(ref customer)).Returns(true);
-//            Assert.IsTrue(logMock.Object.LogWithRefObj(ref customer));
-//            Assert.IsFalse(logMock.Object.LogWithRefObj(ref customerNotUsed));
-//        }
-//        [Test]
+        [Fact]
 
-//        public void BankLogDummy_SetAndGetLogTypeAndSeverityMock_MockTest()
-//        {
-//            var logMock = new Mock<ILogBook>();
-//            logMock.SetupAllProperties();
-//            logMock.Setup(x => x.LogSeverity).Returns(10);
-//            logMock.Setup(x => x.LogType).Returns("warning");
-//            //logMock.Object.LogSeverity = 100;
-//            Assert.That(logMock.Object.LogSeverity, Is.EqualTo(10));
-//            Assert.That(logMock.Object.LogType, Is.EqualTo("warning"));
+        public void BankLogDummy_LogRefChecker_ReturnTrue()
+        {
+            var logMock = new Mock<ILogBook>();
+            Customer customer = new();
+            Customer customerNotUsed = new();
+            logMock.Setup(x => x.LogWithRefObj(ref customer)).Returns(true);
+            Assert.True(logMock.Object.LogWithRefObj(ref customer));
+            Assert.False(logMock.Object.LogWithRefObj(ref customerNotUsed));
+        }
+        [Fact]
 
-//            //Callback with string
-//            string logTemp = "Hello, ";
-//            logMock.Setup(x => x.LogToDB(It.IsAny<string>())).Returns(true)
-//                .Callback((string str) => logTemp += str);
-//            logMock.Object.LogToDB("Anil");
+        public void BankLogDummy_SetAndGetLogTypeAndSeverityMock_MockTest()
+        {
+            var logMock = new Mock<ILogBook>();
+            logMock.SetupAllProperties();
+            logMock.Setup(x => x.LogSeverity).Returns(10);
+            logMock.Setup(x => x.LogType).Returns("warning");
+            //logMock.Object.LogSeverity = 100;
+            Assert.Equal(10,logMock.Object.LogSeverity);
+            Assert.Equal("warning",logMock.Object.LogType);
 
-//            Assert.That(logTemp, Is.EqualTo("Hello, Anil"));
+            //Callback with string
+            string logTemp = "Hello, ";
+            logMock.Setup(x => x.LogToDB(It.IsAny<string>())).Returns(true)
+                .Callback((string str) => logTemp += str);
+            logMock.Object.LogToDB("Anil");
+
+            Assert.Equal("Hello, Anil",logTemp);
 
 
-//            //Callback with integer
-//            int counter = 5;
-//            logMock.Setup(x => x.LogToDB(It.IsAny<string>())).Returns(true)
-//                .Callback(() => counter++);
-//            logMock.Object.LogToDB("Anil");
-//            Assert.That(counter, Is.EqualTo(6));
+            //Callback with integer
+            int counter = 5;
+            logMock.Setup(x => x.LogToDB(It.IsAny<string>())).Returns(true)
+                .Callback(() => counter++);
+            logMock.Object.LogToDB("Anil");
+            Assert.Equal(6,counter);
 
-//        }
+        }
 
-//        [Test]
-//        public void BankLogDummy_VerifyExample()
-//        {
-//            var logMock = new Mock<ILogBook>();
-//            BankAccount bankAccount = new(logMock.Object);
+        [Fact]
+        public void BankLogDummy_VerifyExample()
+        {
+            var logMock = new Mock<ILogBook>();
+            BankAccount bankAccount = new(logMock.Object);
 
-//            bankAccount.Deposit(100);
+            bankAccount.Deposit(100);
 
-//            // Veification
-//            logMock.Verify(u => u.Message(It.IsAny<string>()), Times.Exactly(2));
-//            logMock.Verify(u => u.Message("Test"), Times.AtLeastOnce);
-//            logMock.VerifySet(u => u.LogSeverity = 101, Times.Once);
-//            logMock.VerifyGet(u => u.LogSeverity, Times.Once);
-//        }
-//    }
-//}
+            // Veification
+            logMock.Verify(u => u.Message(It.IsAny<string>()), Times.Exactly(2));
+            logMock.Verify(u => u.Message("Test"), Times.AtLeastOnce);
+            logMock.VerifySet(u => u.LogSeverity = 101, Times.Once);
+            logMock.VerifyGet(u => u.LogSeverity, Times.Once);
+        }
+    }
+}
 
--- SparkyXUnitTest/CustomerXUnitTests.cs
+++ SparkyXUnitTest/CustomerXUnitTests.cs
@@ -16,26 +16,25 @@ namespace Sparky
         {
             customer = new Customer();
         }
-        //[Fact]
-        //public void GreetAndCombineName_InputFistNameAndLastName_GetFullName()
-        //{
-        //    //Arrange
+        [Fact]
+        public void GreetAndCombineName_InputFistNameAndLastName_GetFullName()
+        {
+            //Arrange
 
-        //    //Act
-        //    var fullName = customer.GreetAndCombineNames("Anil", "Yadav");
+            //Act
+            var fullName = customer.GreetAndCombineNames("Anil", "Yadav");
 
-        //    //Assert
-        //    Assert.Multiple(() =>
-        //    {
-        //        Assert.AreEqual("Hello, Anil Yadav", fullName);
-        //        Assert.That(fullName, Is.EqualTo("Hello, Anil Yadav"));
-        //        Assert.That(fullName, Does.Contain("anil Yadav").IgnoreCase);
-        //        Assert.That(fullName, Does.StartWith("Hello,"));
-        //        Assert.That(fullName, Does.EndWith("Yadav"));
-        //        Assert.That(fullName, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
-        //    });
+            //Assert
+           
+            Assert.Equal("Hello, Anil Yadav",fullName);
+            
+            Assert.Contains(("anil Yadav").ToLower(), fullName.ToLower());
+            Assert.StartsWith("Hello,", fullName);
+            Assert.EndsWith("Yadav",fullName);
+            Assert.Matches(("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"), fullName);
+            
 
-        //}
+        }
 
         [Fact]
         public void GreetMessage_NotGreeted_ReturnNull()
@@ -48,12 +47,13 @@ namespace Sparky
             Assert.Null(customer.GreetMessage);
         }
 
-        //[Fact]
-        //public void CheckDiscount_DefaultCustomer_ReturnDiscountInRange()
-        //{
-        //    var result = customer.Discount;
-        //    Assert.InRange(result, Is.InRange(10, 25));
-        //}
+        [Fact]
+        public void CheckDiscount_DefaultCustomer_ReturnDiscountInRange()
+        {
+            var result = customer.Discount;
+            Assert.InRange(result,10, 25);
+        }
+
         [Fact]
         public void GreetMessage_GreetedWithoutLastName_ReturnNotNull()
         {
@@ -63,29 +63,22 @@ namespace Sparky
             Assert.False(string.IsNullOrEmpty(customer.GreetMessage));
         }
 
-        //[Fact]
-        //public void GreetChecker_EmptyFirstName_ThrowException()
-        //{
-        //    // Type 1
-        //    var exceptionDetails = Assert.Throws<ArgumentException>(() =>
-        //    {
-        //        customer.GreetAndCombineNames("", "Yadav");
-        //    });
-        //    Assert.AreEqual("Empty First Name", exceptionDetails.Message);
+        [Fact]
+        public void GreetChecker_EmptyFirstName_ThrowException()
+        {
+            // Type 1
+            var exceptionDetails = Assert.Throws<ArgumentException>(() =>
+            {
+                customer.GreetAndCombineNames("", "Yadav");
+            });
+            Assert.Equal("Empty First Name", exceptionDetails.Message);
 
-        //    //Type 2
-        //    Assert.That(() => customer.GreetAndCombineNames("", "Yadav"),
-        //        Throws.ArgumentException.With.Message.EqualTo("Empty First Name"));
+            // Type 3
+            Assert.Throws<ArgumentException>(() => customer.GreetAndCombineNames("", "Yadav"));
 
-        //    // Type 3
-        //    Assert.Throws<ArgumentException>(() => customer.GreetAndCombineNames("", "Yadav"),
-        //        Throws.ArgumentException.With.Message.EqualTo("Empty First Name").ToString());
+           
 
-        //    //Type 4
-        //    Assert.That(() => customer.GreetAndCombineNames("", "Yadav"),
-        //        Throws.ArgumentException);
-
-        //}
+        }
 
         [Fact]
         public void CustomerType_CreateCustomerWithLessThan100Order_ReturnBasicCustomer()
